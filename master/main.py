6ead87aaa3e755f0be97984d56a233851ddd7aec
kozlopaw <pawel.kozlowski@tieto.com>
2015-07-30 10:21:29 +0200

    master: struct.pack

--- master/main.py
+++ master/main.py
@@ -1,64 +1,79 @@
-import socket, asyncore, struct
 
-BUTTON_USER_EVENT = "/dev/input/event1"
+class ClientUdp(object):
+	def __init__(self, IP, PORT):
+		self.__IP = IP
+		self.__PORT = PORT
+		self.__sock = None
+		self.open_socket()
+		
+	def open_socket(self):
+		self.__server_address = (self.__IP,self.__PORT)
+		self.__sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM):
+		if self.__sock:
+			print("Open success")
+		else:
+			print ("Socket error")
 
-class ReadButton(object):
-   def __init__(self, server_address, sock):
-      print("Start Read Button")
-      self.__sock = sock
-      self.__server_address = server_address
-      self.__file = open(BUTTON_USER_EVENT, "rb")
-      print("File opened")
-      if self.__file:
-         self.__event = self.__file.read(16)
-         self.read_event()
-      else:
-         return 0
+	def send_message(self,MSG):
+		sent = self.__sock.sendto(bytes(str(MSG),'UTF-8'), self.__server_address)
+		print("Message sent")
+	
+	def read_message(self):
+		data,server = self.__sock.recvfrom(1048)
+		print("Received message: "+ data+", from :"+server)
+	
+	
+class EventHandler(object):
+	def __init__(self,PATH):
+		self.__PATH = PATH
+		self.__file = None
+		self.__event = None
+		
+	def open_file(self):
+		self.__file = open(self.__PATH, "rb")
+		if self.__file:
+			self.__event = self.__file.read(16)
+			print("File opened")
+		else:
+			print("File error")
+		
+	def change_status(self):
+		while self.__event:
+			(time1, time2, type, code, value) = struct.unpack('iihhi', self.__event)
+			if type == 1 and code == 276 and value == 1:
+			#send_message
+			#wait_for_response
+		self.close_file()
+	
+	def close_file(self):
+		self.__file.close()	
+	
+	def get_status(self):
+		#send_message
+		#wait_for_response
+		
 
-   def read_event(self):
-      print("Event reading")
-      while self.__event:
-          (time1, time2, type, code, value) = struct.unpack('iihhi', self.__event)
-          if type == 1 and code == 276 and value == 1:
-             sent = self.__sock.sendto(bytes(str(value),'UTF-8'), self.__server_address)
-             print ("Sent byte")  
-          data,server = self.__sock.recvfrom(2048)
-          print ("Recived " + data)
-      file.close()
-      return 1
+class EventManager(object):
+	def __init__(self):
+		self.__queue = []
+		self.__package = None
+
+	def append(self,aDATA):
+		self.__queue.append(aDATA)
+	
+	def pop(self):
+		return self.__queue.pop(0)
+	
+	def make_pack(self):
+		struct.pack("")
+	
+	
+class Master(object):
+	def __init__(self):
+	
+	
 
 
-class ClientUdp(ReadButton):
-   def __init__(self, IP, PORT):
-      print("StartUDP")                                
-      self.__IP = IP
-      self.__PORT = PORT
-      self.__sock = None
-      self.__server_address = (self.__IP,self.__PORT)
-      if self.open_socket():
-         self.ReadButton = ReadButton(self.__server_address, self.__sock)
-      else:
-         print("Cannot open socket")
-               
-   def open_socket(self):
-      self.__sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-      if self.__sock:
-         return True
-      else:
-         return False
-   
-
-class StartClient(ClientUdp):
-   def __init__(self,IP,PORT):
-      print("ClientInit")
-      self.__IP = IP
-      self.__PORT = PORT
-      self.ClientUdp = ClientUdp(self.__IP,self.__PORT)
-      
-
-      
+  
 if __name__=="__main__":
-   
-   StartClient("10.0.0.2",8080)
-
- 
+	StartClient("10.0.0.2",8080)
