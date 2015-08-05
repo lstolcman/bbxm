2b975d829cc0d1eafa3330e52dc8d6062148d956
kozlopaw <pawel.kozlowski@tieto.com>
2015-08-05 13:28:05 +0200

    master: fix button bug + led state

--- master/main.py
+++ master/main.py
@@ -8,32 +8,60 @@ import configparser
 import enum
 import struct
 
-
-
-
 inputQueue = queue.Queue()
 outputQueue = queue.Queue()
+ledQueue = queue.Queue()
 
 cfg = configparser.ConfigParser()
 
+class Led():
+    def __init__(self):
+        self.__led = open("/sys/class/leds/beagleboard::usr0/brightness",'w')
+        self.delay = 3
+        self.state = 0
+        
+    def loop(self):
+        while 1:
+            if not ledQueue.empty():
+                self.state = ledQueue.get()
+            if self.state is 0:
+                self.__led.write('0')
+                self.__led.flush()
+            if self.state is 1:
+                self.__led.write('1')
+                self.__led.flush()
+            if self.state is 2:
+                self.blink(0.8)
+            if self.state is 3:
+                self.blink(0.1)
+                
+    def blink(self,delay):
+        time.sleep(delay)
+        self.__led.write('0')
+        self.__led.flush()
+        time.sleep(delay)
+        self.__led.write('1')
+        self.__led.flush()
+
+
 class Button():
     def __init__(self):
-        self.__PATH = "/dev/input/event0"
+        self.__PATH = "/dev/input/event1"
         self.__file = None
         self.__event = None
         self.packet = Packet()
    
     def loop(self):
         self.__file = open(self.__PATH, "rb")
-        if self.__file:
+        self.__event = self.__file.read(16)
+        while self.__event:
+            (time1, time2, type, code, value) = struct.unpack('iihhi', self.__event)
+            if type == 1 and code == 276 and value == 1:
+                print("Button pressed")
+                ledQueue.put(2)
+                self.packet.parse(0x03)
             self.__event = self.__file.read(16)
-            while self.__event:
-                (time1, time2, type, code, value) = struct.unpack('iihhi', self.__event)
-                if type == 1 and code == 276 and value == 1:
-                    print("Button pressed")
-                    self.packet.put_packet(0x03)
-                self.__event = self.__file.read(16)
-            self.__file.close()
+        self.__file.close()
 
 class Packet():
     def __init__(self):
@@ -46,8 +74,10 @@ class Packet():
             print("Client status")
             if self.packet[0][2] is 0x01: #U-Boot                               
                 print('U-Boot')
+                ledQueue.put(0)
             elif self.packet[0][2] is 0x02: #Linux                              
                 print('Linux')
+                ledQueue.put(1)
 
         if packetType is 0x02:
             print("Client response")
@@ -103,18 +133,20 @@ if __name__ == '__main__':
 
     async = Async(cfg.get('Common', 'Host'), cfg.getint('Common', 'Port'))
     packet = Packet()
-    #button = Button()
+    button = Button()
+    led = Led()
     
     asyncThread = threading.Thread(target=asyncore.loop, kwargs={'timeout':0.1, 'use_poll':True})
     packetThread = threading.Thread(target=packet.loop)
-    #buttonThread = threading.Thread(target=button.loop)
+    buttonThread = threading.Thread(target=button.loop)
+    ledThread = threading.Thread(target=led.loop)
     
     asyncThread.start()
     packetThread.start()
-    #buttonThread.start()
-
+    buttonThread.start()
+    ledThread.start()
 
     print('mainloop')
     while 1:
         packet.parse(0x04)
-        time.sleep(3)
+        time.sleep(10)
